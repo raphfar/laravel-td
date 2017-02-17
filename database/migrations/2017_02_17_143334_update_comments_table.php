1c09ffb408affc35ab4c8815b70fb4de000397df
Raphaël <raphael.farina@devinci.fr>
2017-02-17 15:37:39 +0100

    table migration commentaires

---
+++ database/migrations/2017_02_17_143334_update_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->integer('article_id')->unsigned()->after('user_id');
+            $table->foreign('article_id')->references('id')->on('articles');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropForeign('comments_article_id_foreign');
+            $table->dropForeign('comments_user_id_foreign');
+        });
+    }
+}
