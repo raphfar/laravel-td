7009c2152c11b743b81a7cf19f5ec62420992b7c
Raphaël <raphael.farina@devinci.fr>
2017-02-17 15:57:38 +0100

    Comment / user .php

---
+++ app/Comment.php
@@ -0,0 +1,32 @@
+
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $fillable = [
+        'content','user_id','article_id',
+    ];
+
+    /**
+     * Get the comments for the blog post.
+     */
+
+    public function article() {
+        return $this->belongsTo('App\Article');
+    }
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -30,4 +30,8 @@ class User extends Authenticatable
     public function articles() {
         return $this->hasMany('App\Article');
     }
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
