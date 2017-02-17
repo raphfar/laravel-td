24f92d9a8c4a78eb3169caa16e57d011a30a5385
Raphaël <raphael.farina@devinci.fr>
2017-02-17 16:00:15 +0100

    ajout du many to many dans article

--- app/Article.php
+++ app/Article.php
@@ -11,4 +11,8 @@ class Article extends Model
     public function user() {
         return $this->belongsTo('App\User');
     }
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
 }
