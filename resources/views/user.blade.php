7a7d70d3e360dc50be0c51e7638006ee2d53bc99
Raphaël <raphael.farina@devinci.fr>
2017-02-19 00:01:14 +0100

    profil

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -5,43 +5,32 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    @if(session('success'))
-                        <div class="alert alert-success">
-                            {{session('success')}}
-                        </div>
-                    @endif
-                    <div class="panel-heading">Dashboard</div>
-
-<h3>Vos informations</h3>
-<ul><li>{{ Auth::user()->name }}</li><li>{{ Auth::user()->email }}</li><li>{{ Auth::user()->created_at }}</li></ul>
+                    <div class="panel-heading">Profil</div>
 
                     <div class="panel-body">
-                        <h1>Mes articles</h1>
+                        @if(Auth::check())
+                            <h3>Vos informations</h3>
+                            <ul>
+                                <li>{{ Auth::user()->name }}</li>
+                                <li>{{ Auth::user()->email }}</li>
+                                <li>{{ Auth::user()->created_at }}</li>
+                            </ul>
 
-                        @forelse(Auth::user()->articles as $article)
-                            <h2>{{$article->title}}</h2>
-                            <p>{{$article->content}}</p>
-                        @empty
-                            Aucun article(s)
-                        @endforelse
-
-                        <h1>Mes commentaires</h1>
-                        @forelse(Auth::user()->comments as $comment)
-                            <h3>{{$comment->content}}</h3>
-                            {{ Form::open(['route' => ['users.update', $comment->id], 'method'=> 'put']) }}
-
-                            {{ Form::text('content', '', ['class' => 'form-control']) }}
-
-                            {{ Form::submit('Modifier le commentaire', ['class' => 'btn btn-primary']) }}
-
-                            {{ Form::close() }}
-                        @empty
-                            Aucun commentaire(s)
-                        @endforelse
-
+                            <h3>Vos articles</h3>
+                            <ul>
+                                @forelse(Auth::user()->articles as $article)
+                                    <li><a href="{{ route('article.show', $article->id) }}">{{ $article->title }}</a></li>
+                                @empty
+                                    Vous n'avez pas encore publié d'article.
+                                @endforelse
+                            </ul>
+                        @else
+                            Vous n'êtes pas connecté.
+                        @endif
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
